4d2cc53a888ce715f710db2acc0ab8a55b7007dc
webpon <2249096563@qq.com>
2023-11-12 12:44:47 +0800

    -

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -484,8 +484,9 @@ onUnmounted(() => {
                       <span class="text-purple-500">
                         请<span class="text-red-500">不要使用梯子🪜魔法上网</span>，不然回答有可能卡住！
                       </span>
-                    </h3>
-                    <span class="leading-8 text-base">此版本为免费体验版，高级版本请访问：<a class="underline text-blue-500" href="https://p1.xjai.pro">https://p1.xjai.pro</a></span><br>
+                    </h3><br>
+                    <span class="leading-10 text-xl text-red-500 font-bold">收藏导航页不迷路：<a class="underline text-blue-500" href="https://a.x-code.fun">https://a.x-code.fun</a></span><br>
+                    <span class="text-base">此版本为免费体验版，高级高级版本请访问：<a class="underline text-blue-500" href="https://p1.xjai.pro">https://p1.xjai.pro</a></span><br>
                   <NDivider class="!my-1" />
                   <h3 class="text-xl leading-8">
                     站长推荐<span class="text-red-500">(广告)</span>：<br>
