61473e6fa3542c58c2f330dbaf2ad4541459bc4a
webpon <2249096563@qq.com>
2023-11-22 17:01:22 +0800

    -

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -87,7 +87,9 @@ let api: ChatGPTAPI | ChatGPTUnofficialProxyAPI
     apiModel = 'ChatGPTUnofficialProxyAPI'
   }
 })()
-
+function randomTrueWithProbability(probability) {
+  return Math.random() < probability
+}
 async function chatReplyProcess(options: RequestOptions) {
   const { message, lastContext, process, systemMessage, temperature, top_p } = options
   try {
@@ -105,18 +107,24 @@ async function chatReplyProcess(options: RequestOptions) {
       else
         options = { ...lastContext }
     }
-    let retryCount = 3
+    const retryCount = 3
     for (let i = 0; i < retryCount; i++) {
       try {
-         const response = await api.sendMessage(message, {
-              ...options,
-              onProgress: (partialResponse) => {
-                process?.(partialResponse)
-              },
-            })
-         if (i > 0)
-              console.log(`失败重试第${i}次成功了`)
-            return
+        const response = await api.sendMessage(message, {
+          ...options,
+          onProgress: (partialResponse) => {
+            process?.(partialResponse)
+          },
+        })
+        if (randomTrueWithProbability(0.2)) {
+          const tips = `\n_______________________\n 服务器昂贵,接口昂贵,但网站免费！！如果你觉得做的好，可以给我买一瓶冰阔落
+                ![赞赏码](https://file.xjai.top/uploads/2023-11-22-1700641947344-56063092-image.png)\n
+                `
+          res.write(tips)
+        }
+        if (i > 0)
+          console.log(`失败重试第${i}次成功了`)
+        return
         return sendResponse({ type: 'Success', data: response })
       }
       catch (error) {
