15cc47c8ed26c26a32012477e2a3162584da55f7
webpon <2249096563@qq.com>
2023-11-25 23:09:07 +0800

    -

--- service/src/index.ts
+++ service/src/index.ts
@@ -47,6 +47,7 @@ router.post('/chat-process', [auth, limiter], async (req, res) => {
       tips = `\n_______________________\n
 **目前模型已升级为GPT4, 免费无限制使用！**
 * 防止迷路，请务必收藏导航页：[永久导航页](http://a.x-code.fun)
+* GPT使用教程：[链接](https://b23.tv/IbrTYhn)
       `
     }
     requests[key].length += 1
