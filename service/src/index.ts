f80e728e673751efff84f1698093f63e78d3889d
webpon <2249096563@qq.com>
2023-12-01 01:05:46 +0800

    -

--- service/src/index.ts
+++ service/src/index.ts
@@ -36,10 +36,11 @@ router.post('/chat-process', [auth, limiter], async (req, res) => {
     }
     // * GPT4免费无限制(广告)：[gpt4](http://gnomic.x-code.fun)
     // requests[key] = requests[key].filter(timestamp => timestamp > (currentTimestamp - windowMs))
+    // 服务器昂贵,接口昂贵,但网站免费！！你的捐款是我坚持下去的动力！
     if (requests[key].length % 4 === 0) {
       tips = `\n_______________________\n
-服务器昂贵,接口昂贵,但网站免费！！你的捐款是我坚持下去的动力！
-      ![赞赏码](https://file.xjai.top/uploads/2023-11-22-1700641947344-56063092-image.png)
+支付宝放水了，支付宝扫码领红包！
+      ![赞赏码](https://file.xjai.top/uploads/2023-11-30-1701363862391-31237133-5e4f8ca579fab96c5e8b0d0affc8c99.jpg)
 * 国内稳定高级版本1(广告)：[动点AI](https://p1.xjai.pro)
 * 国内稳定高级版本2(广告)：[YAI](http://yk.x-code.fun)
 * 国内稳定高级版本3(广告)：[ChatAI中文](https://srv.aiflarepro.com/#/?cid=4111)
