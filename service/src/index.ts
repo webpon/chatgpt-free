ae4b49a9b92c6d42d89e7ec2ccca34bf09955178
webpon <2249096563@qq.com>
2024-01-02 17:49:53 +0800

    -

--- service/src/index.ts
+++ service/src/index.ts
@@ -43,8 +43,6 @@ router.post('/chat-process', [auth, limiter], async (req, res) => {
     // * 国内免费GPT4 二(广告)：[YMI_AI](http://yk.x-code.fun)
     if (requests[key].length % 2 === 0) {
       tips = `\n_______________________\n
-服务器昂贵,接口昂贵,但网站免费！！您的捐款是我坚持下去的动力！
-![赞赏码](https://file.xjai.top/uploads/2023-12-10-1702225292439-50015283-%C3%A5%C2%BE%C2%AE%C3%A4%C2%BF%C2%A1%C3%A5%C2%9B%C2%BE%C3%A7%C2%89%C2%87_20231211002121.jpg)
 * 国内免费GPT4(广告)：[百宝箱AI](https://bao.xjai.pro)
 * 国内免费GPT4(广告)：[gnomic](http://gnomic.x-code.fun)
 * 国内稳定GPT4 一(广告)：[动点AI](https://p1.xjai.pro)
