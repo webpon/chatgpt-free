bb82f8213e422e57108b21b34612254aed946721
webpon <2249096563@qq.com>
2024-01-08 10:54:45 +0800

    -

--- service/src/index.ts
+++ service/src/index.ts
@@ -41,21 +41,20 @@ router.post('/chat-process', [auth, limiter], async (req, res) => {
     // 服务器昂贵,接口昂贵,但网站免费！！你的捐款是我坚持下去的动力！
     // * 国内稳定GPT4 一(广告)：[ChatAI中文](https://srv.aiflarepro.com/#/?cid=4111)
     // * 国内免费GPT4 二(广告)：[YMI_AI](http://yk.x-code.fun)
-    //     if (requests[key].length % 2 === 0) {
-    //       tips = `\n_______________________\n
-    // * 国内免费GPT4(广告)：[百宝箱AI](https://bao.xjai.pro)
-    // * 国内免费GPT4(广告)：[gnomic](http://gnomic.x-code.fun)
-    // * 国内稳定GPT4 一(广告)：[动点AI](https://p1.xjai.pro)
-    // * 国内稳定GPT4 二(广告)：[ChatAI中文](https://srv.aiflarepro.com/#/?cid=4111)
-    // * 国内免费GPT4 三(广告)：[YMI_AI](http://yk.x-code.fun)
-    //  `
-    //     }
+    if (requests[key].length % 2 === 0) {
+      tips = `\n_______________________\n
+* 国内免费GPT4(广告)：[gnomic](http://gnomic.x-code.fun)
+* 国内免费GPT4(广告)：[百宝箱AI](https://bao.xjai.pro)
+* 国内稳定GPT4 一(广告)：[动点AI](https://p1.xjai.pro)
+* 国内稳定GPT4 二(广告)：[ChatAI中文](https://srv.aiflarepro.com/#/?cid=4111)
+* 国内免费GPT4 三(广告)：[YMI_AI](http://yk.x-code.fun)
+ `
+    }
     //     else {
-    tips = `\n_______________________\n
+    //       tips = `\n_______________________\n
 
-国内免费GPT4(广告)：[百宝箱AI](https://bao.xjai.pro)
-  `
-    // }
+    // * chatgpt视频教程：[点击观看](https://www.bilibili.com/video/BV1ug4y117iK/?share_source=copy_web&vd_source=7030318409433afd688a7e764aa205f8)`
+    //     }
     requests[key].length += 1
     app.locals.requests = requests
     await chatReplyProcess({
