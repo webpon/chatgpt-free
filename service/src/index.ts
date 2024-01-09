b507c1b31826861c8ced3cf00c3df18348805eda
webpon <2249096563@qq.com>
2024-01-09 12:26:22 +0800

    -

--- service/src/index.ts
+++ service/src/index.ts
@@ -47,7 +47,6 @@ router.post('/chat-process', [auth, limiter], async (req, res) => {
 * 国内免费GPT4(广告)：[百宝箱AI](https://bao.xjai.pro)
 * 国内稳定GPT4 一(广告)：[动点AI](https://p1.xjai.pro)
 * 国内稳定GPT4 二(广告)：[ChatAI中文](https://srv.aiflarepro.com/#/?cid=4111)
-* 国内免费GPT4 三(广告)：[YMI_AI](http://yk.x-code.fun)
  `
     }
     //     else {
