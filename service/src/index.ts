055825aeeae358d6fb2116f30601c6a48d5e8e9c
1 <>
2024-01-15 22:54:11 +0800

    -

--- service/src/index.ts
+++ service/src/index.ts
@@ -44,9 +44,9 @@ router.post('/chat-process', [auth, limiter], async (req, res) => {
     if (requests[key].length % 2 === 0) {
       tips = `\n_______________________\n
 * 国内免费GPT4(广告)：[gnomic](http://gnomic.x-code.fun)
-* 国内免费GPT4(广告)：[百宝箱AI](https://bao.xjai.pro)
-* 国内稳定GPT4 一(广告)：[动点AI](https://p1.xjai.pro)
-* 国内稳定GPT4 二(广告)：[ChatAI中文](https://srv.aiflarepro.com/#/?cid=4111)
+* 国内稳定GPT4 一(广告)：[ChatAI中文](https://srv.aiflarepro.com/#/?cid=4111)
+* 国内稳定GPT4 二(广告)：[百宝箱AI](https://bao.xjai.pro)
+* 国内稳定GPT4 三(广告)：[动点AI](https://p1.xjai.pro)
  `
     }
     //     else {
