cec84379142566973df0d6727c7b9769c124cc48
1 <>
2024-02-15 05:56:55 +0800

    -

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -58,7 +58,7 @@ let api: ChatGPTAPI | ChatGPTUnofficialProxyAPI
         options.maxResponseTokens = 2048
       }
       if (model.toLowerCase().includes('preview')) {
-        options.maxModelTokens = 98304
+        options.maxModelTokens = 16384
         options.maxResponseTokens = 4096
     }
     }
