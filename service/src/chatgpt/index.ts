7865de1462dc5e575ce1b02c43aeef75500a9cbf
1 <>
2024-03-11 21:47:34 +0800

    -

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -98,11 +98,11 @@ async function chatReplyProcess(options: RequestOptions) {
   try {
     let options: SendMessageOptions = { timeoutMs }
 
-    if (apiModel === 'ChatGPTAPI') {
-      if (isNotEmptyString(systemMessage))
-        options.systemMessage = systemMessage
-      options.completionParams = { model, temperature, top_p }
-    }
+    // if (apiModel === 'ChatGPTAPI') {
+    //   if (isNotEmptyString(systemMessage))
+    //     options.systemMessage = systemMessage
+    //   options.completionParams = { model, temperature, top_p }
+    // }
 
     if (lastContext != null) {
       if (apiModel === 'ChatGPTAPI')
@@ -114,7 +114,8 @@ async function chatReplyProcess(options: RequestOptions) {
     for (let i = 0; i < retryCount; i++) {
       try {
         console.log(message)
-
+        console.log(options);
+        
         const response = await api.sendMessage(message, {
           ...options,
           onProgress: (partialResponse) => {
